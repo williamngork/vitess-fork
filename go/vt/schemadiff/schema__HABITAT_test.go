8fdece1fd869d84653b1f613dd8af6b885ab2bee
WilliamNg <williamngow2328@gmail.com>
2025-12-15 15:25:49 +0800

    test

--- go/vt/schemadiff/schema__HABITAT_test.go
+++ go/vt/schemadiff/schema__HABITAT_test.go
@@ -6,13 +6,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewSchemaFromQueriesViewWithCTEFail__HABITAT(t *testing.T) {
-	queries := []string{"create view v30 as with vcte as (select 1) select * from vcte2"}
-	_, err := NewSchemaFromQueries(NewTestEnv(), queries)
-	assert.Error(t, err)
-	assert.EqualError(t, err, (&ViewDependencyUnresolvedError{View: "v30", MissingReferencedEntities: []string{"dual", "vcte2"}}).Error())
-}
-
 func TestNewSchemaFromQueriesViewWithCTE__HABITAT(t *testing.T) {
 	tcases := []struct {
 		name    string
